bcf22edf8279e12ec8972aef6badc51fbfbffb74
ayush <ayushrathore0909@gmail.com>
2025-03-06 23:48:51 +0530

    add

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def call(string ImageName){
+def call(String ImageName){
     echo 'Building the project'
     sh "docker build -t ${ImageName} ."
     echo 'Project built'
--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(string url,string branch){
+def call(String url,String branch){
     echo 'Cloning the repository'
     git url: "${url}", branch: "${branch}"
     echo 'Repository cloned'
