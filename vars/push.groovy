f69e71967a015294bf11112fdf5cb30be7dceed7
Ayush Rathore <109935149+ayushrathore09@users.noreply.github.com>
2025-03-07 00:18:25 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,8 +1,8 @@
 def call(String imagename, String dockerhubuser){
-  sh "pushing the code into the dcoekr hub repository."
+  sh "pushing the code into the docker hub repository."
   sh "docker tag ${imagename} ${dockerhubuser}/${imagename}:latest"
-  withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-    sh "docker login -u ${env.USERNAME} -p ${env.PASSWORD}"
-  }
-  sh "docker push ${imagename} ${dockerhubuser}/${imagename}:latest"
+  // withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+  //   sh "docker login -u ${env.USERNAME} -p ${env.PASSWORD}"
+  // }
+  // sh "docker push ${imagename} ${dockerhubuser}/${imagename}:latest"
 } 
