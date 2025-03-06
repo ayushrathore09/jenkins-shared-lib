aae9e2851885cb2a72e364394d03af208a7b8e38
ayush <ayushrathore0909@gmail.com>
2025-03-07 00:00:27 +0530

    add

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,4 @@
-def call(String imagename,){
+def call(String imagename){
   sh "pushing the code into the dcoekr hub repository."
   withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
     sh "docker login -u ${env.USERNAME} -p ${env.PASSWORD}"
