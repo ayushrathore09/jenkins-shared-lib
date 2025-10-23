7c25fea9acbb269fa64dcfd7853a463402c2bec9
ayush <ayushrathore0909@gmail.com>
2025-10-23 23:35:32 +0530

    add

--- vars/code_push.groovy
+++ vars/code_push.groovy
@@ -1,7 +1,8 @@
 def call (String imagetag, String user){
     echo "Code Push Stage"
     withCredentials([usernamePassword(credentialsId:'dockerHub',usernameVariable:'dockerHubUser',passwordVariable:'dockerHubPass')]){
-                    sh "docker login -u ${user} -p ${env.dockerHubPass}"
+                    //sh "docker login -u ${user} -p ${env.dockerHubPass}"
+                    sh '''echo ${dockerHubPass} | docker login -u ${dockerHubUser} --password-stdin'''
                     // using -p in cli is insecure but for demo purpose we are using it.
                     // also: sh 'docker login -u ${dockerHubUser} -p ${dockerHubPass}' will work interpolatoion won't happen inside single quotes it passes as it is to shell and shell will look for env vars, still -p is insecure.
                     // so will pass silently: sh ''' echo ${dockerHubPass} | docker login -u ${dockerHubUser} --password-stdin ''' passing password through std. input is more secure.
